3dc1b5cdddbe34ceda7856d0f62ea453011ab208
FrankLikuohao <frank.likuohao@gmail.com>
2017-03-14 19:54:20 +0800

    第一版

---
+++ Example01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            status response = status.Success;
+            switch (response)
+            {
+                case status.Success:
+                    //你要寫的程式
+                    Console.WriteLine("狀態成功");
+                    Console.WriteLine((int)response);
+                    break;
+
+                case status.Error:
+                    //你要寫的程式
+                    Console.WriteLine("狀態錯誤");
+                    Console.WriteLine(response);
+                    break;
+
+                default:
+                    //你要寫的程式
+                    Console.WriteLine("未知狀態");
+                    Console.WriteLine(response);
+                    break;
+            }
+        }
+    }
+}
---
+++ Example01/status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal enum status
+    {
+        Success = 3, Error = 5
+    }
+}
